b76e6d72de1141a3834455b381f57691afeafcf2
rafaelM <rafaelmaxwell.silva@gmail.com>
2025-10-04 12:34:05 -0300

    Projeto de estudo atualizado

--- javascript/javascript-amazon-03/scripts/amazon.js
+++ javascript/javascript-amazon-03/scripts/amazon.js
@@ -1,19 +1,120 @@
 // Imports
 import { products } from '../data/products.js';
+import { addToCart, updateCartQuantity} from '../data/cart.js';
 
 // Variables
+
+const cartHtml = document.querySelector('.js-cart-quantity');
 const htmlContainer = document.querySelector('.js-html-container');
 let pageHtml = '';
+let timeoutId;
 
 // Generating HTML
+
+cartHtml.innerHTML = updateCartQuantity();
 
 products.forEach(product => {
 
   const id = product.id;
-  const img = product.image;
   const name = product.name;
-  const price = ${( product.priceCents / 100 ).toFixed(2)};
+  const image = product.image;
+  const price = `$${(product.priceCents / 100).toFixed(2)}`;
+  const stars = (product.rating.stars * 10);
+  const count = product.rating.count;
 
+    pageHtml += 
+  `
+    <div class="product-container js-product-container" data-id="${id}">
+      <div class="product-image-container">
+        <img class="product-image"
+        src="${image}">
+      </div>
+
+    <div class="product-name limit-text-to-2-lines">
+      ${name}
+    </div>
+
+      <div class="product-rating-container">
+        <img class="product-rating-stars"
+        src="images/ratings/rating-${stars}.png">
+        <div class="product-rating-count link-primary">
+        ${count}
+        </div>
+      </div>
+
+    <div class="product-price">
+      ${price}
+    </div>
+
+    <div class="product-quantity-container">
+    <select class="js-select">
+      <option selected value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
+      <option value="8">8</option>
+      <option value="9">9</option>
+      <option value="10">10</option>
+    </select>
+    </div>
+
+    <div class="product-spacer"></div>
+
+    <div class="added-to-cart js-added-message">
+      <img src="images/icons/checkmark.png">
+        Added
+    </div>
+
+    <button class="add-to-cart-button button-primary"
+    data-type="add-button">
+      Add to Cart
+    </button>
+  </div>
+
+`
+  htmlContainer.innerHTML = pageHtml;
 })
 
 // Event Listeners
+
+const productContainer = document.querySelectorAll('.js-product-container');
+
+productContainer.forEach(container => {
+
+  container.addEventListener('click', (event) => {
+
+    const {id} = container.dataset;
+    const target = event.target;
+    const buttonType = target.dataset.type; 
+    const message = container.querySelector('.js-added-message');
+    
+    if(!buttonType) {
+      return;
+    }
+
+    if(buttonType === 'add-button') {
+      const selectElement = container.querySelector('.js-select')
+      let quantity = selectElement.value;
+      quantity = Number(quantity);
+
+      addToCart(id, quantity);
+      popUpMessage(message);
+    }
+
+    cartHtml.innerHTML = updateCartQuantity();
+  })
+})
+
+function popUpMessage(message) {
+
+  message.classList.add('show');
+
+  timeoutId = setTimeout(() => {
+    message.classList.remove('show');
+  }, 2000)
+
+}
+
---
+++ javascript/javascript-amazon-03/scripts/checkout.js
@@ -0,0 +1,171 @@
+// Imports
+import { cart } from '../data/cart.js';
+import { products } from '../data/products.js';
+
+// Variables
+const htmlContainer = document.querySelector('.js-order-summary');
+let pageHtml = '';
+
+// Generating HTML
+
+cart.forEach(cartItem => {
+
+  const getInfo = products.find(product => cartItem.id === product.id);
+
+  const id = getInfo.id;
+  const name = getInfo.name;
+  const image = getInfo.image; 
+  const price = `$${(getInfo.priceCents / 100).toFixed(2)}`
+  const quantity = cartItem.quantity;
+
+  pageHtml += 
+
+      `<div class="cart-item-container js-item-container" data-id="${id}">
+        <div class="delivery-date">
+          Delivery date: Tuesday, June 21
+        </div>
+
+        <div class="cart-item-details-grid">
+          <img class="product-image"
+            src="${image}">
+
+          <div class="cart-item-details">
+            <div class="product-name">
+              ${name}
+            </div>
+            <div class="product-price">
+              ${price}
+            </div>
+            <div class="product-quantity">
+              <span>
+                Quantity: <span class="quantity-label">${quantity}</span>
+              </span>
+              <span class="js-update-quantity update-quantity-link link-primary" 
+              data-type="update">
+                Update
+              </span>
+
+              <div class="js-container-input container-input">
+                  <input type="number" min="1" max="10" class="js-input-quantity input-quantity">
+                  <button type="submit" class="link-primary" data-type="save">
+                    Save
+                  </button>
+              </div>
+
+              <span class="delete-quantity-link link-primary" 
+              data-type="delete">
+                Delete
+              </span>
+            </div>
+          </div>
+
+          <div class="delivery-options">
+            <div class="delivery-options-title">
+              Choose a delivery option:
+            </div>
+            <div class="delivery-option">
+              <input type="radio" checked
+                class="delivery-option-input"
+                name="delivery-option-${id}">
+              <div>
+                <div class="delivery-option-date">
+                  Tuesday, June 21
+                </div>
+                <div class="delivery-option-price">
+                  FREE Shipping
+                </div>
+              </div>
+            </div>
+            <div class="delivery-option">
+              <input type="radio"
+                class="delivery-option-input"
+                name="delivery-option-${id}">
+              <div>
+                <div class="delivery-option-date">
+                  Wednesday, June 15
+                </div>
+                <div class="delivery-option-price">
+                  $4.99 - Shipping
+                </div>
+              </div>
+            </div>
+            <div class="delivery-option">
+              <input type="radio"
+                class="delivery-option-input"
+                name="delivery-option-${id}">
+              <div>
+                <div class="delivery-option-date">
+                  Monday, June 13
+                </div>
+                <div class="delivery-option-price">
+                  $9.99 - Shipping
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> `
+
+    htmlContainer.innerHTML = pageHtml;
+
+})
+
+// Event Listener
+
+const itemContainer = document.querySelectorAll('.js-item-container');
+
+itemContainer.forEach(container => {
+
+  container.addEventListener('click', (event) => {
+
+    const {id} = container.dataset;
+    const target = event.target;
+    const button = target.dataset.type; 
+
+    if (!button) {
+      return;
+    }
+
+    handleButton(container, button);
+
+  })
+
+})
+
+function handleButton(container, button) {
+
+  const upButton = container.querySelector('.js-update-quantity');
+  const inputContainer = container.querySelector('.js-container-input');
+  const {id} = container.dataset;
+
+  if(button === 'update') {
+
+    upButton.classList.add('hidden');
+    inputContainer.classList.add('show');
+
+  } else if(button === 'save') {
+
+    const inputElement = inputContainer.querySelector('.js-input-quantity');
+    let quantity = inputElement.value;
+    quantity = Number(quantity);
+
+      if(quantity === 0) {
+        return;
+      }
+
+      const item = cart.find(cartItem => id === cartItem.id); 
+
+      if(item) {
+        item.quantity = quantity;
+           
+      }
+
+    inputElement.value = '';
+    upButton.classList.remove('hidden');
+    inputContainer.classList.remove('show');
+
+  } else if (button === 'delete') {
+
+  }
+
+}
